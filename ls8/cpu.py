7ac38a935f2c4bd8308b52771c70604317834d1e
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2020-02-25 18:10:48 -0600

    Sanity Commit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,23 +20,42 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        if len(sys.argv) < 2:
+            print('No program provided')
+            exit(2)
+
+        try:
+            address = 0
+            fp = open(sys.argv[1], 'r')
+            for line in fp:
+                if line[0] == '#':
+                    continue
+                instruction = line.split('#')
+                instruction = instruction[0].strip()
+                if instruction == "":
+                    continue
+                self.ram[address] = int(instruction, 2)
+                address += 1
+
+        except FileNotFoundError:
+            print(f"File not found: {sys.argv[1]}")
+            exit(2) 
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -85,4 +104,10 @@ class CPU:
             elif self.ram[self.pc] == 0b01000111:
                 print(self.reg[self.ram_read(self.pc+1)])
                 self.pc += 2
+            #MUL
+            elif self.ram[self.pc] == 0b10100010:
+                operand_1 = self.reg[self.ram_read(self.pc+1)]
+                operand_2 = self.reg[self.ram_read(self.pc+2)]
+                self.reg[self.ram_read(self.pc+1)] = operand_1 * operand_2
+                self.pc += 3
 
